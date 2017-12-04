78d07045642196a892fbad6f03fbf17a00965674
A. Thompson <act.webdesigns@gmail.com>
2017-12-04 16:58:04 -0600

    It runs! Needed: Click event, Results Section and function, and styling

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,159 +1,162 @@
 var trivia = [{
-    question: "What does the acronym USB stand for when referring to a computer port?",
-    answerSet: {
-        A: "User Should Buy",
-        B: "Universal Serial Bus",
-        C: "Uber Stretch Bus",
-        D: "United States Baseball"
+        question: "What does the acronym USB stand for when referring to a computer port?",
+        answerSet: {
+            A: "User Should Buy",
+            B: "Universal Serial Bus",
+            C: "Uber Stretch Bus",
+            D: "United States Baseball"
+        },
+        correctAnswer: "Universal Serial Bus"
     },
-    correctAnswer: "Universal Serial Bus"
-},
-{
-    question: "What do you call the small image icons used to express emotions or ideas in digital communication?",
-    answerSet: {
-        A: "Emojis",
-        B: "GIF's",
-        C: "JPEG's",
-        D: "Texts from Mom"
+    {
+        question: "What do you call the small image icons used to express emotions or ideas in digital communication?",
+        answerSet: {
+            A: "Emojis",
+            B: "GIF's",
+            C: "JPEG's",
+            D: "Texts from Mom"
+        },
+        correctAnswer: "Emojis"
     },
-    correctAnswer: "Emojis"
-},
-{
-    question: "When referring to a computer monitor, what does the acronym LCD stand for?",
-    answerSet: {
-        "A": "Limited Cypher Ducks",
-        "B": "Light Collision Detector",
-        "C": "Laser Color Display",
-        "D": "Liquid Crystal Display"
+    {
+        question: "When referring to a computer monitor, what does the acronym LCD stand for?",
+        answerSet: {
+            "A": "Limited Cypher Ducks",
+            "B": "Light Collision Detector",
+            "C": "Laser Color Display",
+            "D": "Liquid Crystal Display"
+        },
+        correctAnswer: "Liquid Crystal Display"
     },
-    correctAnswer: "Liquid Crystal Display"
-},
-{
-    question: "Which of these is NOT a operating system?",
-    answerSet: {
-        A: "Windows",
-        B: "Android",
-        C: "Linux",
-        D: "Ledges"
+    {
+        question: "Which of these is NOT a operating system?",
+        answerSet: {
+            A: "Windows",
+            B: "Android",
+            C: "Linux",
+            D: "Ledges"
+        },
+        correctAnswer: "Ledges"
     },
-    correctAnswer: "Ledges"
-},
-{
-    question: "When referring to computer memory, what does that acronym RAM stand for?",
-    answerSet: {
-        A: "Real Awesome Memory",
-        B: "Random Access Memory",
-        C: "Razor Alignment Memory",
-        D: "Reacting Automitic Memory"
+    {
+        question: "When referring to computer memory, what does that acronym RAM stand for?",
+        answerSet: {
+            A: "Real Awesome Memory",
+            B: "Random Access Memory",
+            C: "Razor Alignment Memory",
+            D: "Reacting Automitic Memory"
+        },
+        correctAnswer: "Random Access Memory"
     },
-    correctAnswer: "Random Access Memory"
-},
-{
-    question: "In computer science, what does \"GUI\" stand for?",
-    answerSet: {
-        A: "Graphical User Interface",
-        B: "Gooey Ugly Invention",
-        C: "GUI Ducks",
-        D: "Green Umpire Instincts"
+    {
+        question: "In computer science, what does \"GUI\" stand for?",
+        answerSet: {
+            A: "Graphical User Interface",
+            B: "Gooey Ugly Invention",
+            C: "GUI Ducks",
+            D: "Green Umpire Instincts"
+        },
+        correctAnswer: "Graphical User Interface"
     },
-    correctAnswer: "Graphical User Interface"
-},
-{
-    question: "Steve Jobs, Steve Wozniak, and Ronald Wayne founded what company in 1976?",
-    answerSet: {
-        A: "BMW",
-        B: "Apple",
-        C: "The Linux Foundation",
-        D: "Microsoft"
+    {
+        question: "Steve Jobs, Steve Wozniak, and Ronald Wayne founded what company in 1976?",
+        answerSet: {
+            A: "BMW",
+            B: "Apple",
+            C: "The Linux Foundation",
+            D: "Microsoft"
+        },
+        correctAnswer: "Microsoft"
     },
-    correctAnswer: "Microsoft"
-},
-{
-    question: "In a website browser address bar what does \"www\" stand for?",
-    answerSet: {
-        A: "We Will Win",
-        B: "Wuhwuhwuh",
-        C: "World Wide Web",
-        D: "Walter Went Wild"
+    {
+        question: "In a website browser address bar what does \"www\" stand for?",
+        answerSet: {
+            A: "We Will Win",
+            B: "Wompa Wompa Wompa",
+            C: "World Wide Web",
+            D: "Walter Went Wild"
+        },
+        correctAnswer: "World Wide Web"
     },
-    correctAnswer: "World Wide Web"
-},
-{
-    question: "HTML and CSS are computer languages used to create what?",
-    answerSet: {
-        A: "Operating Systems",
-        B: "Keyboards",
-        C: "Web Pages",
-        D: "Machine Learning Algorithims"
+    {
+        question: "HTML and CSS are computer languages used to create what?",
+        answerSet: {
+            A: "Operating Systems",
+            B: "Keyboards",
+            C: "Web Pages",
+            D: "Machine Learning Algorithims"
+        },
+        correctAnswer: "Web Pages"
     },
-    correctAnswer: "Web Pages"
-},
-{
-    question: "In a photo editing program, what do the letters RGB stand for?",
-    answerSet: {
-        A: "Red, Green, Blue",
-        B: "Remove Giant Background",
-        C: "Ready, Go, Begin",
-        D: "Rare Gem Broker"
-    },
-    correctAnswer: "Red, Green, Blue"
-},
-{
-    question: "When talking about computer memory, what does the acronym ROM stand for?",
-    answerSet: {
-        A: "Remote Operated Monkeys",
-        B: "Rigid Operator Mistakes",
-        C: "Run of mistakes",
-        D: "Read Only Memory"
-    },
-    correctAnswer: "Read Only Memory"
-}
+    {
+        question: "In a photo editing program, what do the letters RGB stand for?",
+        answerSet: {
+            A: "Red, Green, Blue",
+            B: "Remove Giant Background",
+            C: "Ready, Go, Begin",
+            D: "Rare Gem Broker"
+        },
+        correctAnswer: "Red, Green, Blue"
+    }
 ];
 
 // Starting Countdown Functions
-var time = 6;
+var startTime = 6;
 var initialCounter = setInterval(initialCountdown, 1000);
 function initialCountdown() {
-    time--;
-    if (time <= 0) {
+    startTime--;
+    if (startTime === 0) {
         clearInterval(initialCountdown);
         initGame();
         return;
     }
-    $("#initial-countdown").text(time);
+    $("#initial-countdown").text(startTime);
 }
 
-
 // Game Runtime
-var numCorrect, numIncorrect = 0;
+// this needs to go in somewhere i'm sure  --> var numCorrect, numIncorrect = 0;
 function initGame() {
     $("#welcome").hide();
-    var questionSpace = $("#question");
-    var answerChoiceSpace = $("#choices");
-    var gameTimer = setInterval(runTrivia, 10000);
-
-    // Add Answer Space HTML element for each t.answerSet
-    // Create event listener for clicking t.answerSet element
-    // Finish runTrivia()
+    var gameTime = 6; //21 for actual game
+    var currentQuestion = 0;
+    var gameTimer = setInterval(runTrivia, 1000);
+    var totalTime = 0; // Time Test
     function runTrivia() {
-        var currentQuestion = 0;
-
-        var triviaStr = trivia[currentQuestion];
-        var answerSet = triviaStr.answerSet;
-        if (currentQuestion <= 9) {
-            currentQuestion++;
-            questionSpace.text(triviaStr.question);
-            jQuery.each(answerSet, function (i, val) {
-                $("#" + i).text(val);
-            });
-        } else {
+        // If it doesn't test variables first it won't reach currentQuestion = 10; 
+        if (currentQuestion === 10) {
+            console.log("Game completed in  " + totalTime + " seconds.");
             clearInterval(runTrivia);
             return;
-        }
+        } else if (gameTime == 0) {
+            gameTime += 5;
+            $("#game").hide("slow");
+            console.log("Question " + currentQuestion + " finished in " + totalTime); //Test
+            return currentQuestion++;
+        };
+
+        var questionSpace = $("#question");
+        var answerChoiceSpace = $("#choices");
+        var triviaStr = trivia[currentQuestion];
+        var answerSet = triviaStr.answerSet;
+        gameTime--; // Keeps time for each interval
+        totalTime++; // Time Test
+        
+        // Updates the DOM for each question
+        $("#game-timer").html(gameTime);
+        $("#question-num").html(currentQuestion + 1);
+        questionSpace.text(triviaStr.question);
+        jQuery.each(answerSet, function (i, val) {
+            $("#" + i).text(i + ": " + val);
+        });
+        $("#game").show("slow"); // Works best here (after intial DOM update)
+        $("#result-section").show();
+        
     }
 }
 
+
+
 // add a button that fires this function in the results div
 function restartGame() {
-    setInterval(initialCountdown, 1000);
+    location.reload();
 }
